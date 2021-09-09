9a9576a98e9f9a8067f8728bac13ffead5a2058e
pd-newton <pd-newton@ponddy-edu.net>
2021-09-09 16:15:07 +0800

    feat: nginx

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+echo $CIRCLE_PROJECT_REPONAME
+cd $CIRCLE_PROJECT_REPONAME
+export CIRCLE_PROJECT_REPONAME=$CIRCLE_PROJECT_REPONAME
+pwd
+git pull
+
+mkdir -p ../conf.d
+pwd
+CONF_FILE=`ls ./*.conf`
+echo $CONF_FILE
+for CONF in $CONF_FILE
+do
+    mv $CONF ../conf.d
+done
+ls
+
+mkdir -p ../docker-compose
+envsubst < docker-compose-template.yml > docker-compose-$CIRCLE_PROJECT_REPONAME.yml
+cp docker-compose-$CIRCLE_PROJECT_REPONAME.yml ../docker-compose
+ls
+
+cd ../docker-compose
+pwd
+FILENAME=`ls ./*.yml`
+echo $FILENAME
+FILE="docker-compose"
+for EACHFILE in $FILENAME
+do
+    FILE+=" -f $EACHFILE"
+done
+FILE+=" up -d"
+echo FILE
+
+echo $FILE
+echo $FILE > run.sh
+sh run.sh
